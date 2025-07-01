4aa8e25babe3b2b6d44d32f38a8a239559de753e
Dushyant Singh Rajput <dushysingh@gmail.com>
2025-06-30 21:01:24 -0400

    Time: 31 ms (57.01%) | Memory: 51.4 MB (83.07%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,67 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         //Base Case
+//         if(nums == null || nums.length < 3) return new ArrayList();
+//         //Storing the result
+//         Set<List<Integer>> result = new HashSet<>();
+//         //Sorting the array
+//         Arrays.sort(nums);
+
+//         for(int i=0; i<nums.length-2; i++) {         //i<arr.length-2 so that we can form atleast a triplet
+//             int j = i+1;
+//             int k = nums.length-1;
+
+//             while(j<k) {
+//                 int sum = nums[i] + nums[j] + nums[k];
+//                 if(sum == 0) {
+//                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+//                     j++;
+//                     k--;
+//                 } 
+//                 else if(sum < 0)
+//                     j++;
+//                 else
+//                     k--;
+//             }
+//         }
+//         return new ArrayList(result);
+//     }
+// }
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //Base Case
-        if(nums == null || nums.length < 3) return new ArrayList();
-        //Storing the result
-        Set<List<Integer>> result = new HashSet<>();
-        //Sorting the array
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+
         Arrays.sort(nums);
 
-        for(int i=0; i<nums.length-2; i++) {         //i<arr.length-2 so that we can form atleast a triplet
-            int j = i+1;
-            int k = nums.length-1;
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while(j<k) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0) {
+                if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // Skip duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+
                     j++;
                     k--;
-                } 
-                else if(sum < 0)
+                } else if (sum < 0) {
                     j++;
-                else
+                } else {
                     k--;
+                }
             }
         }
-        return new ArrayList(result);
+
+        return result;
     }
 }
