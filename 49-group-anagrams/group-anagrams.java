086d471e5995e7503b6a99f07695680b26fca86b
Dushyant Singh Rajput <dushysingh@gmail.com>
2025-07-23 19:14:37 -0400

    Time: 18 ms (23.41%) | Memory: 49.2 MB (10.15%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,12 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        //Store result in a HashMap
         Map<String, List<String>> result = new HashMap<>();
         for(String s: strs) {
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedString = new String(charArray);
-            result.putIfAbsent(sortedString, new ArrayList());
-            result.get(sortedString).add(s);
+            int[] count = new int[26];
+            /* Change each string value to character array and use ASCII values
+            to check the frequency of each character for each string in strs */
+            for(char c: s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            result.putIfAbsent(key, new ArrayList());
+            result.get(key).add(s);
         }
         return new ArrayList(result.values());
     }
